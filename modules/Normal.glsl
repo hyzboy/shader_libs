17a89c10ae9e55eb960569a7ddb5c9ec1d317d22
hyzboy <hyzboy@gmail.com>
2020-10-22 12:03:05 +0800

    delete normal.glsl module

--- modules/Normal.glsl
+++
@@ -1,9 +0,0 @@
-mat3 GetWorldNormalMatrix()
-{
-    return mat3(transpose(inverse(world.modelview*pc.local_to_world)));
-}
-
-vec3 GetWorldNormal()
-{
-    return normalize(GetWorldNormalMatrix()*Normal);
-}
