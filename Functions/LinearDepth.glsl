aa71bf9d956c5ecc5a846dc93539e64365ef5f3a
hyzboy <hyzboy@gmail.com>
2021-05-06 22:25:53 +0800

    1.add LinearDepth.glsl 2.add znear/zfar in WorldMatrix.xml 3.update MVPmatrix use in Texture3D.vert.xml

--- Functions/LinearDepth.glsl
+++ Functions/LinearDepth.glsl
@@ -4,5 +4,5 @@
 float linearDepth(float depth)
 {
 	float z = depth * 2.0f - 1.0f;
-	return (2.0f * ubo.nearPlane * ubo.farPlane) / (ubo.farPlane + ubo.nearPlane - z * (ubo.farPlane - ubo.nearPlane));
+	return (2.0f * camera.znear * camera.zfar) / (camera.zfar + camera.znear - z * (camera.zfar - camera.znear));
 }
