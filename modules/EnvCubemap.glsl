6d874f5b82026471ccbc07cabf26ce546b6176d3
hyzboy <hyzboy@gmail.com>
2022-01-11 19:28:35 +0800

    finished envcubemap material

--- modules/EnvCubemap.glsl
+++ modules/EnvCubemap.glsl
@@ -1,11 +1,9 @@
-vec3 GetEnvCubemapCoord(vec3 dir,vec3 normal)
+/**
+ * 获取Cubemap环境反射纹理坐标
+ * @param nw_pos normalize化过的世界坐标
+ * @param nw_normal normalize化过的世界法线
+ */
+vec3 GetEnvCubemapCoord(vec3 nw_pos,vec3 nw_normal)
 {
-    vec3 I = normalize(dir);
-    vec3 R = reflect (I*vec3(1,-1,1), normal);
-
-    R=vec3(inverse(r_scene_info.l2w)*vec4(R,0.0));
-    // Convert cubemap coordinates into Vulkan coordinate space
-    R.y*=-1.0;
-
-    return R.xzy;
+    return reflect(nw_pos*vec3(1.0,-1.0,1.0),nw_normal).xzy*vec3(-1.0,-1.0,1.0);
 }
